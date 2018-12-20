e43a1e73fe5f1700505acea6bf9dcf9aa1741d9a
kimberly-moors <kimberly.moors@gmail.com>
2018-12-21 00:23:49 +0100

    Update Python

--- Data.py
+++ Data.py
@@ -2,9 +2,10 @@ import csv
 import pandas as pd
 import sklearn as sk
 import numpy as np
-from sklearn.cross_validation import train_test_split
+from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.preprocessing import LabelEncoder as le
+from sklearn.preprocessing import LabelEncoder
 
 if __name__ == "__main__":
 
@@ -12,10 +13,11 @@ if __name__ == "__main__":
 	dataset= pd.read_csv(file)
 	X = dataset.loc[:, ['STATUS']]
 	y= dataset.cEXT	
-	y= le.fit_transform(dataset['cEXT'],y)				
+	le = LabelEncoder()
+	y= le.fit_transform(dataset['cEXT'])		
 
 	#train-test-split
-	X1,X2,y1,y2=train_test_split(X,y, random_state=1, train_size=0.9)
+	X1,X2,y1,y2=train_test_split(X,y, random_state=1, train_size=0.9, test_size=0.1)
 	trainingset= X1
 	testset= X2
 
