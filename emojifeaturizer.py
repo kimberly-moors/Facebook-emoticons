f0d60cb0382137f80c0a670d0165c277c699bd4f
kimberly-moors <kimberly.moors@gmail.com>
2019-04-03 16:58:47 +0200

    Update emojifeaturizer.py
    Correction

--- emojifeaturizer.py
+++ emojifeaturizer.py
@@ -1,37 +1,58 @@
 def emoji_featurizer (dataset):
-	#the emoji's placed into a dictionary.
+	"""This function creates a featureset of emoticons. Here the function iterates
+	over every instance of a dataset and counts the number of emoticons for each column;
+	(happy, random, winking, neutral, sad, angry). Additionally, a column is created that 
+	represent the sum of all the emoticons.
+
+	Parameters: 
+	dataset =  a dataset consisting of a column ('STATUS') that exists of texutal data, possibly 
+	containing emoticons.
+	"""
+
 	emojidictionary = {}
-	emojidictionary['happy'] = [':)',':]','XD',':-)','^_^',':D',':P','xD',':}',':>', '=)']
-	emojidictionary['random'] = ['<3',':$',':o']
-	emojidictionary['winking'] = [';)',';-)']
-	emojidictionary['neutral'] = [':|','-.-']
-	emojidictionary['sad'] = [':(',':[',':-(','=(',':{','://',":\'(",':c']
-	emojidictionary['angry'] = ['^^',':@',':S','>:(','-_-']
+	emojidictionary['Happy'] = [':)',':]','XD',':-)','^_^',':D',':P','xD',':}',':>', '=)']
+	emojidictionary['Random'] = ['<3',':$',':o']
+	emojidictionary['Winking'] = [';)',';-)']
+	emojidictionary['Neutral'] = [':|','-.-']
+	emojidictionary['Sad'] = [':(',':[',':-(','=(',':{','://',":\'(",':c']
+	emojidictionary['Angry'] = ['^^',':@',':S','>:(','-_-']
 
+	emoji_data = dataset
 	for key in emojidictionary.keys():
-		dataset.insert(1,key, 0)
-	dataset.insert(1,'EMOJINR', 0)
+		emoji_data.insert(1,key, 0)
+	emoji_data.insert(1,'EMOJINR', 0)
 
-	def features(dictionary, key, dataset):
+	def features(dictionary, key, emoji_data):
+		"""This fuction creates the information for each feature. It counts the
+		number of times a feature appears within each line within the dataset and 
+		places this information into a column that will be added to the dataset.
+
+		parameters:
+		dictionary = a dictionary in with the keys represent the features and the values
+			represent the words that belong to this feature.
+		key = the feature that will be focused on.
+		dataset = a dataset consisting of a column ('STATUS') that exists of texutal data, possibly 
+			containing emoticons
+
+		"""
+		
 		cat = []
-		for line in dataset['STATUS']:
+		for line in emoji_data['STATUS']:
+			line = line.replace(',', ' ')
 			words = line.split(" ")
+			i = 0
 			wordlist = []
 			for word in words:
 				wordlist.append(word)
-				i = 0
-				for value in emojidictionary[key]:
-					if value in wordlist:
-						i += 1
+			for value in wordlist:
+				if value in emojidictionary[key]:
+					i += 1
 			cat.append(i)
-		dataset[key] = cat
-		return (dataset[key])
-	features(emojidictionary, 'happy', dataset)	
-	features(emojidictionary, 'random', dataset)
-	features(emojidictionary, 'winking', dataset)
-	features(emojidictionary, 'neutral', dataset)			
-	features(emojidictionary, 'sad', dataset)
-	features(emojidictionary, 'angry', dataset)	
-	dataset['EMOJINR'] = (dataset['happy']+dataset['sad']+dataset['angry']+dataset['random']+dataset['neutral']+dataset['winking'])
-	del dataset['STATUS']
-	return (dataset)
+		emoji_data[key] = cat
+		return (emoji_data[key])
+
+	for key in emojidictionary.keys():
+		features(emojidictionary, key, emoji_data)	
+	emoji_data['EMOJINR'] = (emoji_data['Happy'] + emoji_data['Sad'] + emoji_data['Angry'] + emoji_data['Random'] + emoji_data['Neutral'] + emoji_data['Winking'])
+	emoji_data =  emoji_data.drop(columns = ['STATUS'])
+	return (emoji_data)
